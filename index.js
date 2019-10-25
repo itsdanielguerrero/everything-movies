faacff6968c394fed1bb9fe6ef95ae5abe4113dc
itsdanielguerrero <itsdanielguerrero@gmail.com>
2019-10-25 15:09:59 -0400

    removed error handler funciton and hashed out some bugs

--- index.js
+++ index.js
@@ -10,38 +10,25 @@ function getDirector(title) {
 }
 
 const DirectorIntentHandler = {
-    canHandle(handlerInput) {
-      return handlerInput.requestEnvelope.request.type === 'IntentRequest' &&
-        handlerInput.requestEnvelope.request.intent.name === 'DirectorIntent'
-    },
-    handle(handlerInput) {
-      const title = handlerInput.requestEnvelope.request.intent.slots.Title.value
-  
-      return getDirector(title).then(function (director) {
-        const speechText = `The director of ${title} is <emphasis>${director}</emphasis>`
-  
-        return handlerInput.responseBuilder
-          .speak(speechText)
-          .getReponse()
-      })
-    }
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest' &&
+      handlerInput.requestEnvelope.request.intent.name === 'DirectorIntent'
+  },
+  handle(handlerInput) {
+    const title = handlerInput.requestEnvelope.request.intent.slots.Title.value
+
+    return getDirector(title).then(function (director) {
+      const speechText = `The director of ${title} is <emphasis>${director}</emphasis>`
+
+      return handlerInput.responseBuilder
+        .speak(speechText)
+        .getReponse()
+    })
   }
-
-const ErrorHandler = {
-    canHandle (){
-        return true
-    },
-    handel(handlerInput) {
-        return handlerInput.responseBuilder
-            .speak('My bad, I did not catch that. Can you repeat it?')
-            .reprompt('My bad, I did not catch that. Can you repeat it?')
-            .withShouldEndSession(false)
-            .getReponse()
-
-    },
 }
+
+const builder = Alexa.SkillBuilders.custom()
 
 exports.handler = builder
   .addRequestHandlers(DirectorIntentHandler)
-  .addErrorHandlers(ErrorHandler)
   .lambda()
